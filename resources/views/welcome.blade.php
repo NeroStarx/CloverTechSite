1fc8e79d0150b0c459025f83fb508ab44c11181b
Aaron NeroStarx <35573892+NeroStarx@users.noreply.github.com>
2020-06-27 22:10:30 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-image: url(asset('assets/img/back-wallpaper.png'))
+                background-image: url("{{asset('assets/img/back-wallpaper.png')}}")
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
