e7b0ddee72adcc45fb293561eddbe6df2921ff95
Aaron NeroStarx <35573892+NeroStarx@users.noreply.github.com>
2020-06-27 23:22:24 +0100

    Update tables.blade.php

--- resources/views/view/tables.blade.php
+++ resources/views/view/tables.blade.php
@@ -12,7 +12,7 @@
                 <div class="card-body">
                   <div class="table-responsive">
                     <table class="table">
-                      <thead class=" text-primary">
+                      <thead class=" text-info">
                         <th>
                           ID
                         </th>
